3ae5f933f4466c7321419fdf1159d187f3531b59
BT_1N3_03 <@>
2018-11-20 09:59:47 -0500

    123

---
+++ src/InPlaceSortsRunner.java
@@ -0,0 +1,9 @@
+public class InPlaceSortsRunner
+{
+    public static void main(String[] args)
+    {
+        InPlaceSorts test1 = new InPlaceSorts();
+        InPlaceSorts test2 = new InPlaceSorts();
+        InPlaceSorts test3 = new InPlaceSorts();
+    }
+}
